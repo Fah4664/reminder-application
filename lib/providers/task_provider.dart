e42677381e12719157f0d8ca714c0af94e46f44d
Ckew2565 <kewalin2284@gmail.com>
2024-07-22 22:04:22 +0700

    Update task_provider.dart

--- lib/providers/task_provider.dart
+++ lib/providers/task_provider.dart
@@ -3,8 +3,11 @@ import '../models/task.dart';
 
 class TaskProvider with ChangeNotifier {
   List<Task> _tasks = [];
+  List<Task> _completedTasks = []; // New list for completed tasks
 
   List<Task> get tasks => _tasks;
+  List<Task> get completedTasks =>
+      _completedTasks; // Getter for completed tasks
 
   void addTask(Task task) {
     _tasks.add(task);
@@ -20,11 +23,16 @@ class TaskProvider with ChangeNotifier {
 
   void removeTask(Task task) {
     _tasks.remove(task);
+    _completedTasks
+        .remove(task); // Ensure task is also removed from completed tasks
     notifyListeners();
   }
 
   void markTaskAsCompleted(Task task) {
-    // Implement completion logic if needed
-    notifyListeners();
+    if (_tasks.contains(task)) {
+      _tasks.remove(task);
+      _completedTasks.add(task); // Add task to completed tasks
+      notifyListeners();
+    }
   }
 }
