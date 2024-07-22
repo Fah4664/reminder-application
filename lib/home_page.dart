3f4b614c7809c31cd2e87c4de55f26a07bdb5bfb
Ckew2565 <kewalin2284@gmail.com>
2024-07-22 22:58:13 +0700

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,3 +1,4 @@
+// pages/home_page.dart
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'providers/task_provider.dart';
@@ -5,6 +6,7 @@ import 'add_task_page.dart';
 import 'search_page.dart';
 import 'view_task_page.dart'; // Import the ViewTasksPage
 import 'edit_task_page.dart'; // Import the EditTaskPage
+import '../models/task.dart'; // Import the Task model
 
 class HomePage extends StatelessWidget {
   @override
@@ -26,87 +28,7 @@ class HomePage extends StatelessWidget {
                   title: Text(task.title),
                   subtitle: Text(task.description),
                   onTap: () {
-                    showDialog(
-                      context: context,
-                      builder: (BuildContext context) {
-                        return AlertDialog(
-                          content: Container(
-                            width: MediaQuery.of(context).size.width * 0.9,
-                            child: Stack(
-                              children: <Widget>[
-                                Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: <Widget>[
-                                    Text(
-                                      task.title,
-                                      style: TextStyle(
-                                          fontSize: 24,
-                                          fontWeight: FontWeight.bold),
-                                    ),
-                                    SizedBox(height: 10),
-                                    Text(
-                                      task.description,
-                                      style: TextStyle(fontSize: 18),
-                                    ),
-                                  ],
-                                ),
-                                Positioned(
-                                  right: 0,
-                                  top: 0,
-                                  child: IconButton(
-                                    icon: Icon(Icons.edit),
-                                    onPressed: () {
-                                      Navigator.of(context).pop(); // Close the dialog
-                                      Navigator.push(
-                                        context,
-                                        MaterialPageRoute(
-                                          builder: (context) => EditTaskPage(
-                                            task: task,
-                                            index: index, // ส่งดัชนีที่นี่
-                                          ),
-                                        ),
-                                      );
-                                    },
-                                  ),
-                                ),
-                              ],
-                            ),
-                          ),
-                          actions: <Widget>[
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                IconButton(
-                                  icon: Icon(Icons.arrow_back),
-                                  onPressed: () {
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    Provider.of<TaskProvider>(context,
-                                            listen: false)
-                                        .markTaskAsCompleted(task);
-                                    Navigator.of(context).pop();
-                                  },
-                                  child: Text('Mark as Completed'),
-                                ),
-                                IconButton(
-                                  icon: Icon(Icons.delete, color: Color.fromARGB(255, 0, 0, 0)),
-                                  onPressed: () {
-                                    Provider.of<TaskProvider>(context,
-                                            listen: false)
-                                        .removeTask(task);
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ],
-                            ),
-                          ],
-                        );
-                      },
-                    );
+                    _showTaskDetailsDialog(context, task, index);
                   },
                 ),
               );
@@ -135,17 +57,27 @@ class HomePage extends StatelessWidget {
         ],
         selectedItemColor: Colors.black,
         unselectedItemColor: Colors.black,
-        onTap: (index) {
+        onTap: (index) async {
           if (index == 1) {
             Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => AddTaskPage()),
             );
           } else if (index == 2) {
-            Navigator.push(
+            final selectedTask = await Navigator.push(
               context,
               MaterialPageRoute(builder: (context) => SearchPage()),
             );
+
+            if (selectedTask != null && selectedTask is Task) {
+              // แสดงรายละเอียดของงานที่เลือก
+              _showTaskDetailsDialog(
+                  context,
+                  selectedTask,
+                  Provider.of<TaskProvider>(context, listen: false)
+                      .tasks
+                      .indexOf(selectedTask));
+            }
           } else if (index == 3) {
             Navigator.push(
               context,
@@ -154,6 +86,87 @@ class HomePage extends StatelessWidget {
           }
         },
       ),
+    );
+  }
+
+  void _showTaskDetailsDialog(BuildContext context, Task task, int index) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          content: Container(
+            width: MediaQuery.of(context).size.width * 0.9,
+            child: Stack(
+              children: <Widget>[
+                Column(
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      task.title,
+                      style:
+                          TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      task.description,
+                      style: TextStyle(fontSize: 18),
+                    ),
+                  ],
+                ),
+                Positioned(
+                  right: 0,
+                  top: 0,
+                  child: IconButton(
+                    icon: Icon(Icons.edit),
+                    onPressed: () {
+                      Navigator.of(context).pop(); // Close the dialog
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EditTaskPage(
+                            task: task,
+                            index: index, // ส่งดัชนีที่นี่
+                          ),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.arrow_back),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                ),
+                TextButton(
+                  onPressed: () {
+                    Provider.of<TaskProvider>(context, listen: false)
+                        .markTaskAsCompleted(task);
+                    Navigator.of(context).pop();
+                  },
+                  child: Text('Mark as Completed'),
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete, color: Colors.red),
+                  onPressed: () {
+                    Provider.of<TaskProvider>(context, listen: false)
+                        .removeTask(task);
+                    Navigator.of(context).pop();
+                  },
+                ),
+              ],
+            ),
+          ],
+        );
+      },
     );
   }
 }
