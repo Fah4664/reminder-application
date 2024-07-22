0987aeba77526e036eabbe2299bf7aecc56c3f9c
Fah4664 <warapornzz4664@gmail.com>
2024-07-22 21:47:42 +0700

    Create edit_task_page.dart

---
+++ lib/edit_task_page.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'providers/task_provider.dart';
+import 'models/task.dart';
+
+class EditTaskPage extends StatefulWidget {
+  final Task task;
+
+  EditTaskPage({required this.task});
+
+  @override
+  _EditTaskPageState createState() => _EditTaskPageState();
+}
+
+class _EditTaskPageState extends State<EditTaskPage> {
+  final _titleController = TextEditingController();
+  final _descriptionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    _titleController.text = widget.task.title;
+    _descriptionController.text = widget.task.description;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('Edit Task')),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              controller: _titleController,
+              decoration: InputDecoration(labelText: 'Title'),
+            ),
+            TextField(
+              controller: _descriptionController,
+              decoration: InputDecoration(labelText: 'Description'),
+            ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pop(); // Close the page
+                  },
+                  child: Text('Cancel'),
+                ),
+                ElevatedButton(
+                  onPressed: () {
+                    Provider.of<TaskProvider>(context, listen: false).updateTask(
+                      widget.task.copyWith(
+                        title: _titleController.text,
+                        description: _descriptionController.text,
+                      ),
+                    );
+                    Navigator.of(context).pop(); // Close the page
+                  },
+                  child: Text('Update'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
