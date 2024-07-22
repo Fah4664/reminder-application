941683323f9f9055dc15d3008e54f0cc702b087d
Fah4664 <warapornzz4664@gmail.com>
2024-07-22 20:44:20 +0700

    Update home_page.dart

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -3,14 +3,13 @@ import 'package:provider/provider.dart';
 import 'add_task_page.dart';
 import 'providers/task_provider.dart';
 import 'search_page.dart'; // Import the SearchPage
-import 'task_detail_page.dart'; // Import the TaskDetailPage
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Reminder App'),
+        title: Center(child: Text('Track Goals')), // Change app name and center it
       ),
       body: Consumer<TaskProvider>(
         builder: (context, taskProvider, child) {
@@ -18,17 +17,58 @@ class HomePage extends StatelessWidget {
             itemCount: taskProvider.tasks.length,
             itemBuilder: (context, index) {
               final task = taskProvider.tasks[index];
-              return ListTile(
-                title: Text(task.title),
-                subtitle: Text(task.description),
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => TaskDetailPage(task: task),
-                    ),
-                  );
-                },
+              return Card( // Wrap ListTile with Card for shadow effect
+                elevation: 4,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                child: ListTile(
+                  title: Text(task.title),
+                  subtitle: Text(task.description),
+                  onTap: () {
+                    showDialog(
+                      context: context,
+                      builder: (BuildContext context) {
+                        return AlertDialog(
+                          title: Text('Task Details'),
+                          content: Container(
+                            width: double.maxFinite, // Expand the width
+                            child: Column(
+                              mainAxisSize: MainAxisSize.min,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text(
+                                  task.title,
+                                  style: TextStyle(
+                                      fontSize: 24, fontWeight: FontWeight.bold),
+                                ),
+                                SizedBox(height: 10),
+                                Text(
+                                  task.description,
+                                  style: TextStyle(fontSize: 18),
+                                ),
+                              ],
+                            ),
+                          ),
+                          actions: <Widget>[
+                            IconButton(
+                              icon: Icon(Icons.arrow_back),
+                              onPressed: () {
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                            IconButton(
+                              icon: Icon(Icons.delete),
+                              onPressed: () {
+                                Provider.of<TaskProvider>(context, listen: false)
+                                    .removeTask(task);
+                                Navigator.of(context).pop();
+                              },
+                            ),
+                          ],
+                        );
+                      },
+                    );
+                  },
+                ),
               );
             },
           );
