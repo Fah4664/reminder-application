b654ab20b5ac0349a4861744dbe50b1d98cf2d93
Fah4664 <warapornzz4664@gmail.com>
2024-07-22 21:56:31 +0700

    Update task_provider.dart

--- lib/providers/task_provider.dart
+++ lib/providers/task_provider.dart
@@ -1,36 +1,30 @@
 import 'package:flutter/foundation.dart';
 import '../models/task.dart';
 
-
 class TaskProvider with ChangeNotifier {
   List<Task> _tasks = [];
-  List<Task> _completedTasks = [];
 
   List<Task> get tasks => _tasks;
-  List<Task> get completedTasks => _completedTasks;
 
   void addTask(Task task) {
     _tasks.add(task);
     notifyListeners();
   }
 
-  void removeTask(Task task) {
-    _tasks.remove(task);
-    _completedTasks.remove(task);
-    notifyListeners();
-  }
-
-  void markTaskAsCompleted(Task task) {
-    if (_tasks.contains(task)) {
-      _tasks.remove(task);
-      _completedTasks.add(task);
+  void updateTask(int index, Task updatedTask) {
+    if (index >= 0 && index < _tasks.length) {
+      _tasks[index] = updatedTask;
       notifyListeners();
     }
   }
 
-  void updateTask(int index, Task updatedTask) {
-    _tasks[index] = updatedTask;
+  void removeTask(Task task) {
+    _tasks.remove(task);
     notifyListeners();
   }
 
+  void markTaskAsCompleted(Task task) {
+    // Implement completion logic if needed
+    notifyListeners();
+  }
 }
