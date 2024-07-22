5f3a01fd931d42f7d9df537589d029242eafff83
Fah4664 <warapornzz4664@gmail.com>
2024-07-22 20:59:46 +0700

    Update task_provider.dart

--- lib/providers/task_provider.dart
+++ lib/providers/task_provider.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/foundation.dart';
 import '../models/task.dart';
 
+
 class TaskProvider with ChangeNotifier {
   List<Task> _tasks = [];
 
-  List<Task> get tasks => _tasks;
+  List<Task> get tasks => _tasks.where((task) => !task.isCompleted).toList(); // Filter out completed tasks
 
   void addTask(Task task) {
     _tasks.add(task);
@@ -14,5 +15,17 @@ class TaskProvider with ChangeNotifier {
   void removeTask(Task task) {
     _tasks.remove(task);
     notifyListeners();
+  }
+
+  void markTaskAsCompleted(Task task) {
+    final index = _tasks.indexOf(task);
+    if (index != -1) {
+      _tasks[index] = Task(
+        title: task.title,
+        description: task.description,
+        isCompleted: true, // Mark task as completed
+      );
+      notifyListeners();
+    }
   }
 }
