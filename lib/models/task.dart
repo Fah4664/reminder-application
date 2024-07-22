c799db85d31b4dc08e78422e5edcae0eab79ec5b
Fah4664 <warapornzz4664@gmail.com>
2024-07-22 20:59:43 +0700

    Update task.dart

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,6 +1,11 @@
 class Task {
-  String title;
-  String description;
+  final String title;
+  final String description;
+  bool isCompleted;
 
-  Task({required this.title, required this.description});
+  Task({
+    required this.title,
+    required this.description,
+    this.isCompleted = false, // Default to not completed
+  });
 }
