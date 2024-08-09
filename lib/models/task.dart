6518ef871ade5c0288da6d6d8d034cb80fdafcbe
Fah4664 <warapornzz4664@gmail.com>
2024-08-10 00:49:24 +0700

    Update task.dart

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -3,11 +3,15 @@ class Task {
   final String title;
   final String description;
   final bool isAllDay; // เพิ่มพารามิเตอร์นี้
+  final DateTime? startDateTime;
+  final DateTime? endDateTime;
 
   Task({
     required this.title,
     required this.description,
     this.isAllDay = false, // ค่าเริ่มต้นเป็น false
+    this.startDateTime,
+    this.endDateTime,
   });
 
   Task copyWith({
@@ -22,4 +26,3 @@ class Task {
     );
   }
 }
-
