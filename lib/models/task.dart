948848026e483588e7e96413473176c64dad0d72
Fah4664 <warapornzz4664@gmail.com>
2024-08-10 00:10:00 +0700

    Update task.dart

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,13 +1,25 @@
+// models/task.dart
 class Task {
   final String title;
   final String description;
+  final bool isAllDay; // เพิ่มพารามิเตอร์นี้
 
-  Task({required this.title, required this.description});
+  Task({
+    required this.title,
+    required this.description,
+    this.isAllDay = false, // ค่าเริ่มต้นเป็น false
+  });
 
-  Task copyWith({String? title, String? description}) {
+  Task copyWith({
+    String? title,
+    String? description,
+    bool? isAllDay, // เพิ่มพารามิเตอร์นี้
+  }) {
     return Task(
       title: title ?? this.title,
       description: description ?? this.description,
+      isAllDay: isAllDay ?? this.isAllDay, // คัดลอกค่าของ isAllDay
     );
   }
 }
+
