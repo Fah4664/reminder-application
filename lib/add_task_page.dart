8be6e733d1520b240e40f8f9b550a97a9bffd295
Ckew2565 <kewalin2284@gmail.com>
2024-08-16 19:58:17 +0700

    Update add_task_page.dart

--- lib/add_task_page.dart
+++ lib/add_task_page.dart
@@ -9,6 +9,7 @@ class AddTaskPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Add Task'),
+        automaticallyImplyLeading: false, // ปิดการแสดงปุ่มย้อนกลับ
       ),
       body: const Padding(
         padding: EdgeInsets.all(16.0),
