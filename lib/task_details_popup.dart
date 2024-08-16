f0ef5227c2a3f1215a332fbc9eebb9c66c6e8688
Fah4664 <warapornzz4664@gmail.com>
2024-08-16 22:43:51 +0700

    Update task_details_popup.dart

--- lib/task_details_popup.dart
+++ lib/task_details_popup.dart
@@ -3,8 +3,10 @@ import 'package:provider/provider.dart';
 import 'providers/task_provider.dart';
 import 'edit_task_page.dart';
 import '../models/task.dart';
+import 'utils/date_utils.dart';
 
 void showTaskDetailsPopup(BuildContext context, Task task, int index) {
+  Color dialogColor = task.color ?? Colors.grey; // Fallback color
   showDialog(
     context: context,
     builder: (BuildContext context) {
@@ -13,6 +15,7 @@ void showTaskDetailsPopup(BuildContext context, Task task, int index) {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(5.0),
         ),
+        backgroundColor: dialogColor, // ใช้สีที่รับเข้ามาจาก Task
         content: SizedBox(
           width: MediaQuery.of(context).size.width * 0.8,
           height: MediaQuery.of(context).size.height * 0.3,
@@ -28,6 +31,7 @@ void showTaskDetailsPopup(BuildContext context, Task task, int index) {
                     style: const TextStyle(
                       fontSize: 22,
                       fontWeight: FontWeight.bold,
+                      color: Color(0xFF000000), // เปลี่ยนสีข้อความให้เหมาะสมกับพื้นหลัง
                     ),
                   ),
                   IconButton( // แสดงหน้าจอการแก้ไข task หรือทำการอัปเดต task ตรงนี้
@@ -44,10 +48,38 @@ void showTaskDetailsPopup(BuildContext context, Task task, int index) {
                   ),
                 ],
               ),
-              const SizedBox(height: 10),
+              const SizedBox(height: 8),
               Text(
                 task.description,
-                style: const TextStyle(fontSize: 18),
+                style: const TextStyle(
+                  fontSize: 18,
+                  color: Color(0xFF000000), // เปลี่ยนสีข้อความให้เหมาะสมกับพื้นหลัง
+                ),
+              ),
+              const SizedBox(height: 8),
+              Container(
+                width: 200,
+                height: 15,
+                decoration: BoxDecoration(
+                  color: const Color(0xFFd0d0d0),
+                  borderRadius: BorderRadius.circular(15.0),
+                ),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Container(
+                    width: (MediaQuery.of(context).size.width - 100) * (task.sliderValue * 100 / 150),
+                    height: 15,
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF717273),
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 5),
+              Text(
+                formatDateRange(task.startDateTime, task.endDateTime),
+                style: const TextStyle(fontSize: 16, color: Color(0xFF000000)), // เปลี่ยนสีข้อความให้เหมาะสมกับพื้นหลัง
               ),
             ],
           ),
@@ -56,7 +88,7 @@ void showTaskDetailsPopup(BuildContext context, Task task, int index) {
           Row(
             children: [
               IconButton(
-                icon: const Icon(Icons.arrow_back),
+                icon: const Icon(Icons.arrow_back, color: Color(0xFF000000)), // เปลี่ยนสีไอคอน
                 onPressed: () {
                   Navigator.of(context).pop();
                 },
@@ -68,18 +100,18 @@ void showTaskDetailsPopup(BuildContext context, Task task, int index) {
                   Navigator.of(context).pop();
                 },
                 style: TextButton.styleFrom(
-                  foregroundColor: const Color(0xFF000000), // กำหนดสีข้อความ
-                  backgroundColor: const Color(0xFFd0d0d0), // กำหนดสีพื้นหลังของปุ่ม
+                  foregroundColor: const Color(0xFF000000), // สีข้อความของปุ่ม
+                  backgroundColor: const Color(0xFFd0d0d0), // สีพื้นหลังของปุ่ม
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(5.0), // กำหนดความโค้งของมุม
+                    borderRadius: BorderRadius.circular(5.0), // ความโค้งของมุมปุ่ม
                   ),
-                  padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 0.5), // กำหนดพื้นที่ภายในปุ่ม
+                  padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 0.5),
                 ),
                 child: const Text('Mark as Completed'),
               ),
               const Spacer(),
               IconButton(
-                icon: const Icon(Icons.delete, color: Color(0xFF000000)),
+                icon: const Icon(Icons.delete, color: Color(0xFF000000)), // เปลี่ยนสีไอคอน
                 onPressed: () {
                   Provider.of<TaskProvider>(context, listen: false).removeTask(task);
                   Navigator.of(context).pop();
