a4a10b392d256aa3074d0a268adfc215c7555e19
Fah4664 <warapornzz4664@gmail.com>
2024-08-16 18:58:33 +0700

    Update task_provider.dart

--- lib/providers/task_provider.dart
+++ lib/providers/task_provider.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 import '../models/task.dart';
 
 class TaskProvider with ChangeNotifier {
-  final List<Task> _activeTasks = []; // รายการสำหรับเก็บ tasks ที่ยังไม่เสร็จ
+  final List<Task> _tasks = []; // รายการสำหรับเก็บ tasks ที่ยังไม่เสร็จ
   final List<Task> _completedTasks = []; // รายการสำหรับเก็บ tasks ที่เสร็จแล้ว
-  List<Task> get tasks => _activeTasks; // รายการ tasks ที่ยังไม่เสร็จ
+  List<Task> get tasks => _tasks; // รายการ tasks ที่ยังไม่เสร็จ
   List<Task> get completedTasks => _completedTasks; // รายการ tasks ที่เสร็จแล้ว
 
   // ฟังก์ชันสำหรับเพิ่มหรืออัปเดต task
   void addTask(Task task) {
-    final index = _activeTasks.indexWhere((t) => t.id == task.id);
+    final index = _tasks.indexWhere((t) => t.id == task.id);
     if (index == -1) {
       // ถ้า task ยังไม่มีในรายการ ให้เพิ่ม task ใหม่
-      _activeTasks.add(task);
+      _tasks.add(task);
     } else {
       // ถ้า task มีอยู่แล้วในรายการ ให้ทำการอัปเดต task ที่มีอยู่
-      updateTask(task); // ใช้ฟังก์ชัน updateTask แทน
+      updateTask(task);
     }
     saveAllTasks(); // บันทึกการเปลี่ยนแปลง
     notifyListeners(); // แจ้งให้ผู้ฟังทราบว่ามีการเปลี่ยนแปลง
@@ -23,9 +23,9 @@ class TaskProvider with ChangeNotifier {
 
   // ฟังก์ชันสำหรับอัปเดต task ที่มีอยู่
   void updateTask(Task updatedTask) {
-    final index = _activeTasks.indexWhere((task) => task.id == updatedTask.id);
+    final index = _tasks.indexWhere((task) => task.id == updatedTask.id);
     if (index != -1) {
-      _activeTasks[index] = updatedTask.copyWith(
+      _tasks[index] = updatedTask.copyWith(
         title: updatedTask.title,
         description: updatedTask.description,
         isAllDay: updatedTask.isAllDay,
@@ -48,15 +48,28 @@ class TaskProvider with ChangeNotifier {
 
   // ฟังก์ชันสำหรับลบ task ออกจากทั้ง _activeTasks และ _completedTasks
   void removeTask(Task task) {
-    _activeTasks.remove(task);
+    _tasks.remove(task);
     _completedTasks.remove(task);
     saveAllTasks(); // บันทึกการเปลี่ยนแปลง
     notifyListeners(); // แจ้งให้ผู้ฟังทราบว่ามีการเปลี่ยนแปลง
   }
 
+  // ฟังก์ชันสำหรับอัปเดตตัวเลือกการแจ้งเตือน
+  void setNotificationOption(String taskId, String newNotificationOption) {
+    final index = _tasks.indexWhere((task) => task.id == taskId);
+    if (index != -1) {
+      final updatedTask = _tasks[index].copyWith(notificationOption: newNotificationOption);
+      _tasks[index] = updatedTask;
+      saveAllTasks(); // บันทึกการเปลี่ยนแปลง
+      notifyListeners(); // แจ้งให้ผู้ฟังทราบว่ามีการเปลี่ยนแปลง
+    } else {
+      throw Exception('ไม่พบงาน: $taskId');
+    }
+  }
+
   // ฟังก์ชันสำหรับทำเครื่องหมายว่า task เสร็จสมบูรณ์
   void markTaskAsCompleted(Task task) {
-    if (_activeTasks.remove(task)) {
+    if (_tasks.remove(task)) {
       _completedTasks.add(task);
       saveAllTasks(); // บันทึกการเปลี่ยนแปลง
       notifyListeners(); // แจ้งให้ผู้ฟังทราบว่ามีการเปลี่ยนแปลง
@@ -65,8 +78,8 @@ class TaskProvider with ChangeNotifier {
 
   // ฟังก์ชันสำหรับอัปเดตความก้าวหน้า (progress) ของ task
   void updateTaskProgress(int index, double newProgress) {
-    if (index >= 0 && index < _activeTasks.length) {
-      _activeTasks[index] = _activeTasks[index].copyWith(goalProgress: newProgress);
+    if (index >= 0 && index < _tasks.length) {
+      _tasks[index] = _tasks[index].copyWith(goalProgress: newProgress);
       saveAllTasks(); // บันทึกการเปลี่ยนแปลง
       notifyListeners(); // แจ้งให้ผู้ฟังทราบว่ามีการเปลี่ยนแปลง
     }
