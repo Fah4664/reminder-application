5c32fb8d752797e12b571b759167e889c6ab5bd9
Fah4664 <warapornzz4664@gmail.com>
2024-08-16 20:02:26 +0700

    Update track_goals_box.dart

--- lib/track_goals_box.dart
+++ lib/track_goals_box.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-// StatefulWidget สำหรับแทร็กเป้าหมาย
 class TrackGoals extends StatefulWidget {
   // รับค่าความก้าวหน้า (progress) และฟังก์ชันที่ใช้ในการแจ้งการเปลี่ยนแปลงของความก้าวหน้า
   final double progress;
@@ -12,20 +11,17 @@ class TrackGoals extends StatefulWidget {
     required this.progress,
     required this.onProgressUpdated,
   });
-
+  
   @override
-  // ignore: library_private_types_in_public_api
   TrackGoalsState createState() => TrackGoalsState();
 }
 
 class TrackGoalsState extends State<TrackGoals> {
-  // ตัวแปรสำหรับเก็บค่าของ Slider
   late double sliderValue;
 
   @override
   void initState() {
     super.initState();
-    // แปลงค่าความก้าวหน้า (progress) จากช่วง 0-1 ไปเป็นเปอร์เซ็นต์ (0-100)
     sliderValue = widget.progress * 100.0; // Convert to percentage
   }
 
@@ -33,65 +29,59 @@ class TrackGoalsState extends State<TrackGoals> {
   Widget build(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(
-        left: 20.0, // ระยะห่างจากด้านซ้าย
-        right: 20.0, // ระยะห่างจากด้านขวา
-        top: 15.0, // ระยะห่างจากด้านบน
-        bottom: 2.0, // ระยะห่างจากด้านล่าง
+        left: 20.0,
+        right: 20.0,
+        top: 15.0,
+        bottom: 2.0,
       ),
       decoration: BoxDecoration(
-        color: Colors.white, // สีพื้นหลังของ Container
-        borderRadius: BorderRadius.circular(15.0), // มุมโค้งมนของ Container
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(15.0),
         boxShadow: [
           BoxShadow(
-            color: Colors.grey.withOpacity(0.2), // สีของเงาที่จางลง
-            blurRadius: 5.0, // รัศมีเบลอของเงา
-            offset: const Offset(0, 2), // การเลื่อนตำแหน่งของเงา
+            color: Colors.grey.withOpacity(0.2),
+            blurRadius: 5.0,
+            offset: const Offset(0, 2),
           ),
         ],
       ),
       child: Column(
-        crossAxisAlignment:
-            CrossAxisAlignment.center, // การจัดแนวเนื้อหาภายในแนวตั้ง
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           const Text(
-            'Track Goals', // ข้อความที่แสดง
-            style: TextStyle(fontSize: 20), // ขนาดตัวอักษร
+            'Track Goals',
+            style: TextStyle(fontSize: 20),
           ),
-          const SizedBox(height: 10), // ระยะห่างระหว่างข้อความกับ Slider
+          const SizedBox(height: 10),
           Row(
             children: <Widget>[
               Expanded(
                 child: SliderTheme(
                   data: SliderThemeData(
-                    activeTrackColor:
-                        const Color(0xFF717273), // สีของแทร็กที่ใช้งานอยู่
-                    inactiveTrackColor:
-                        const Color(0xFFd0d0d0), // สีของแทร็กที่ไม่ใช้งาน
-                    thumbColor: const Color(0xFF4c4949), // สีของลูกบอลบน Slider
-                    overlayColor: const Color(0xFFd0d0d0)
-                        .withAlpha(30), // สีของ overlay บน Slider เมื่อจับ
-                    trackHeight: 10.0, // ความสูงของแทร็ก Slider
+                    activeTrackColor: const Color(0xFF717273),
+                    inactiveTrackColor: const Color(0xFFd0d0d0),
+                    thumbColor: const Color(0xFF4c4949),
+                    overlayColor: const Color(0xFFd0d0d0).withAlpha(30),
+                    trackHeight: 10.0,
                   ),
                   child: Slider(
-                    value: sliderValue, // ค่าเริ่มต้นของ Slider
-                    min: 0.0, // ค่าต่ำสุดของ Slider
-                    max: 100.0, // ค่าสูงสุดของ Slider
-                    divisions: 100, // จำนวนหน่วยที่แยก Slider
+                    value: sliderValue,
+                    min: 0.0,
+                    max: 100.0,
+                    divisions: 100,
                     onChanged: (double value) {
                       setState(() {
-                        // อัปเดตค่า _sliderValue และแจ้งการเปลี่ยนแปลงไปยังพาเรนต์
                         sliderValue = value;
-                        widget.onProgressUpdated(value / 100.0); // แจ้งพาเรนต์ด้วยค่าที่ถูกปรับเป็นช่วง 0-1
+                        widget.onProgressUpdated(value / 100.0); // Notify parent of progress update
                       });
                     },
                   ),
                 ),
               ),
-              const SizedBox(
-                  width: 10), // ระยะห่างระหว่าง Slider กับข้อความเปอร์เซ็นต์
+              const SizedBox(width: 10),
               Text(
-                '${sliderValue.toStringAsFixed(0)}%', // แสดงเปอร์เซ็นต์ที่ได้จาก Slider
-                style: const TextStyle(fontSize: 16), // ขนาดตัวอักษร
+                '${sliderValue.toStringAsFixed(0)}%',
+                style: const TextStyle(fontSize: 16),
               ),
             ],
           ),
