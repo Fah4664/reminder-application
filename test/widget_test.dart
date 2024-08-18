49766292bbe619f5474d93f76fa7e968f9c6f324
Fah4664 <warapornzz4664@gmail.com>
2024-08-18 22:37:05 +0700

    Update widget_test.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:reminder_application/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(ReminderApp());
+    await tester.pumpWidget(const ReminderApp());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
