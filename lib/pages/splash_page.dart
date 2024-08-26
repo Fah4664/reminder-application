dd5fb8774b092610387f1be7610ef57d8987736f
Ckew2565 <kewalin2284@gmail.com>
2024-08-26 17:12:30 +0700

    Create splash_page.dart

---
+++ lib/pages/splash_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'dart:async';
+import 'home_page.dart';
+
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _SplashPageState createState() => _SplashPageState();
+}
+
+class _SplashPageState extends State<SplashPage> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 3), () {
+      Navigator.pushAndRemoveUntil(
+        context,
+        MaterialPageRoute(builder: (context) => const HomePage()),
+        (Route<dynamic> route) => false, // ลบหน้าทั้งหมดใน stack
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Image.asset(
+          'assets/images/reminder.png', // ระบุเส้นทางไฟล์ของรูปภาพ
+          width: 200,
+          height: 200,
+        ),
+      ),
+    );
+  }
+}
