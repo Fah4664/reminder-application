e39acca5bdeda025faa94bd495bfdd7cbde5ce11
Fah4664 <warapornzz4664@gmail.com>
2024-08-26 12:07:50 +0700

    Update task.dart

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+//import 'package:flutter/material.dart';
 
 class Task {
   String id; // เพิ่ม id
@@ -8,7 +8,7 @@ class Task {
   DateTime? startDateTime;
   DateTime? endDateTime;
   String notificationOption;
-  Color? color;
+  String? color; // เปลี่ยนเป็น String สำหรับการจัดเก็บใน Firestore
   double sliderValue;
 
   Task({
@@ -19,12 +19,42 @@ class Task {
     required this.startDateTime,
     required this.endDateTime,
     this.notificationOption = 'None',
-    required this.color,
+    this.color, // ใช้ String แทน Color
     required this.sliderValue,
   });
 
   // Getter สำหรับตรวจสอบสถานะว่า task เสร็จสมบูรณ์หรือไม่
   bool get isCompleted => sliderValue >= 100.0;
+
+  // ฟังก์ชันสำหรับแปลง Task เป็น Map
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'title': title,
+      'description': description,
+      'isAllDay': isAllDay,
+      'startDateTime': startDateTime?.toIso8601String(),
+      'endDateTime': endDateTime?.toIso8601String(),
+      'notificationOption': notificationOption,
+      'color': color,
+      'sliderValue': sliderValue,
+    };
+  }
+
+  // ฟังก์ชันสำหรับสร้าง Task จาก Map
+  factory Task.fromMap(Map<String, dynamic> map) {
+    return Task(
+      id: map['id'] as String,
+      title: map['title'] as String,
+      description: map['description'] as String,
+      isAllDay: map['isAllDay'] as bool? ?? false,
+      startDateTime: map['startDateTime'] != null ? DateTime.parse(map['startDateTime'] as String) : null,
+      endDateTime: map['endDateTime'] != null ? DateTime.parse(map['endDateTime'] as String) : null,
+      notificationOption: map['notificationOption'] as String? ?? 'None',
+      color: map['color'] as String?, // แปลงจาก String เป็น Color เมื่ออ่านจาก Firestore
+      sliderValue: map['sliderValue'] as double,
+    );
+  }
 
   Task copyWith({
     String? id,
@@ -33,8 +63,8 @@ class Task {
     bool? isAllDay,
     DateTime? startDateTime,
     DateTime? endDateTime,
-    String ? notificationOption,
-    Color? color,
+    String? notificationOption,
+    String? color,
     double? sliderValue,
   }) {
     return Task(
