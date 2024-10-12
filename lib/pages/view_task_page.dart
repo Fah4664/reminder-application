926691e5522634f6cdff3d8b7d8cf8e5ac19bacd
Ckew2565 <kewalin2284@gmail.com>
2024-10-12 17:04:03 +0700

    Update view_task_page.dart

--- lib/pages/view_task_page.dart
+++ lib/pages/view_task_page.dart
@@ -1,75 +1,94 @@
-import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import 'package:reminder_application/utils/date_utils.dart';
-import 'add_task_page.dart';
-import '../providers/task_provider.dart';
-import 'home_page.dart';
-import 'search_page.dart'; // เพิ่มการนำเข้า SearchPage
-import '../utils/color_utils.dart';
+import 'package:flutter/material.dart'; // Import Flutter material design library
+import 'package:provider/provider.dart'; // Import provider package for state management
+import 'package:reminder_application/utils/date_utils.dart'; // Import date utility functions
+import 'add_task_page.dart'; // Import the page to add new tasks
+import '../providers/task_provider.dart'; // Import task provider for state management
+import 'home_page.dart'; // Import home page
+import 'search_page.dart'; // Import search page
+import '../utils/color_utils.dart'; // Import color utility functions
 
+// Define the ViewTasksPage as a StatelessWidget
 class ViewTasksPage extends StatelessWidget {
-  const ViewTasksPage({super.key});
+  const ViewTasksPage({super.key}); // Constructor
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Center(child: Text('View Completed Tasks')),
-        automaticallyImplyLeading: false,
+        title:
+            const Center(child: Text('View Completed Tasks')), // AppBar title
+        automaticallyImplyLeading: false, // Do not show leading button
       ),
       body: Consumer<TaskProvider>(
+        // Listen to changes in TaskProvider
         builder: (context, taskProvider, child) {
-          final completedTasks = taskProvider.completedTasks;
+          final completedTasks =
+              taskProvider.completedTasks; // Get completed tasks from provider
           if (completedTasks.isEmpty) {
+            // Check if there are no completed tasks
             return const Center(
-              child: Text('No completed tasks.'),
+              child: Text('No completed tasks.'), // Display message if no tasks
             );
           }
 
           return ListView.builder(
-            itemCount: completedTasks.length,
+            // Create a list view to display completed tasks
+            itemCount: completedTasks.length, // Number of completed tasks
             itemBuilder: (context, index) {
-              final task = completedTasks[index];
+              // Build each item in the list
+              final task =
+                  completedTasks[index]; // Get the task at the current index
               return Card(
-                elevation: 4,
-                color: task.color != null
+                elevation: 4, // Shadow effect for the card
+                color: task.color != null // Use task color if available
                     ? colorFromString(task.color!)
-                    : const Color(0xFFede3e3),
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    : const Color(0xFFede3e3), // Default color if not set
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10, horizontal: 15), // Margin around card
                 child: ListTile(
                   title: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+                    // Column to display task details
+                    crossAxisAlignment:
+                        CrossAxisAlignment.start, // Align text to start
                     children: [
                       Text(
-                        task.title,
+                        task.title, // Display task title
                         style: const TextStyle(
-                            fontSize: 20, fontWeight: FontWeight.bold),
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold), // Title style
                       ),
-                      const SizedBox(height: 5),
+                      const SizedBox(height: 5), // Spacer
                       Text(
                         formatDateRange(task.startDateTime,
-                            task.endDateTime), // แสดงช่วงวันที่
+                            task.endDateTime), // Show date range
                         style: const TextStyle(
-                            fontSize: 16, color: Color(0xFF000000)),
+                            fontSize: 16,
+                            color: Color(0xFF000000)), // Date style
                       ),
-                      const SizedBox(height: 5),
+                      const SizedBox(height: 5), // Spacer
                       Container(
-                        width: 200,
-                        height: 15,
+                        width: 200, // Width of the progress bar
+                        height: 15, // Height of the progress bar
                         decoration: BoxDecoration(
-                          color: const Color(0xFFd0d0d0),
-                          borderRadius: BorderRadius.circular(15.0),
+                          color: const Color(
+                              0xFFd0d0d0), // Background color of the progress bar
+                          borderRadius:
+                              BorderRadius.circular(15.0), // Rounded corners
                         ),
                         child: Align(
-                          alignment: Alignment.centerLeft,
+                          alignment: Alignment
+                              .centerLeft, // Align progress indicator to the left
                           child: Container(
                             width: (MediaQuery.of(context).size.width - 100) *
-                                (task.sliderValue * 100 / 150),
-                            height: 15,
+                                (task.sliderValue *
+                                    100 /
+                                    150), // Calculate width based on slider value
+                            height: 15, // Height of the inner progress bar
                             decoration: BoxDecoration(
-                              color: const Color(0xFF717273),
-                              borderRadius: BorderRadius.circular(15.0),
+                              color:
+                                  const Color(0xFF717273), // Progress bar color
+                              borderRadius: BorderRadius.circular(
+                                  15.0), // Rounded corners
                             ),
                           ),
                         ),
@@ -83,55 +102,60 @@ class ViewTasksPage extends StatelessWidget {
         },
       ),
       bottomNavigationBar: BottomAppBar(
+        // Bottom navigation bar
         child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          mainAxisAlignment:
+              MainAxisAlignment.spaceBetween, // Space out navigation buttons
           children: <Widget>[
-            const SizedBox(width: 48), // ขนาดว่างด้านซ้าย
+            const SizedBox(width: 48), // Empty space on the left
             IconButton(
               icon: Image.asset(
-                'assets/icons/target.png',
+                'assets/icons/target.png', // Icon for home page
                 height: 27,
                 width: 27,
               ),
               onPressed: () {
                 Navigator.push(
+                  // Navigate to HomePage when pressed
                   context,
                   MaterialPageRoute(builder: (context) => const HomePage()),
                 );
               },
             ),
-            const Spacer(),
+            const Spacer(), // Spacer between icons
             IconButton(
               icon: Image.asset(
-                'assets/icons/Add Task.png',
+                'assets/icons/Add Task.png', // Icon for adding a task
                 height: 29,
                 width: 29,
               ),
               onPressed: () {
                 Navigator.push(
+                  // Navigate to AddTaskPage when pressed
                   context,
                   MaterialPageRoute(builder: (context) => const AddTaskPage()),
                 );
               },
             ),
-            const Spacer(),
+            const Spacer(), // Spacer between icons
             IconButton(
               icon: Image.asset(
-                'assets/icons/search.png',
+                'assets/icons/search.png', // Icon for search page
                 height: 28,
                 width: 28,
               ),
               onPressed: () {
                 Navigator.push(
+                  // Navigate to SearchPage when pressed
                   context,
                   MaterialPageRoute(builder: (context) => const SearchPage()),
                 );
               },
             ),
-            const Spacer(),
+            const Spacer(), // Spacer between icons
             IconButton(
               icon: Image.asset(
-                'assets/icons/View Tasks.png',
+                'assets/icons/View Tasks.png', // Icon for current page
                 height: 28,
                 width: 28,
               ),
@@ -139,7 +163,7 @@ class ViewTasksPage extends StatelessWidget {
                 // Stay on this page, do nothing
               },
             ),
-            const SizedBox(width: 48), // ขนาดว่างด้านขวา
+            const SizedBox(width: 48), // Empty space on the right
           ],
         ),
       ),
