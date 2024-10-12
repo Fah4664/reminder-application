5ea98c22a4371244959db710cb2cc877693333a0
Ckew2565 <kewalin2284@gmail.com>
2024-10-12 16:27:22 +0700

    Update login_page.dart

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,48 +1,62 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_auth/firebase_auth.dart';
-import 'home_page.dart';
-import 'register_page.dart';
+import 'package:flutter/material.dart'; // Importing Flutter material design package
+import 'package:firebase_auth/firebase_auth.dart'; // Importing Firebase Authentication package
+import 'home_page.dart'; // Importing HomePage widget
+import 'register_page.dart'; // Importing RegisterPage widget
 
+// The main LoginPage widget, which is a StatefulWidget
 class LoginPage extends StatefulWidget {
   @override
-  _LoginPageState createState() => _LoginPageState();
+  _LoginPageState createState() =>
+      _LoginPageState(); // Creating the state for LoginPage
 }
 
 class _LoginPageState extends State<LoginPage> {
+  // Controllers for email and password input fields
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
+
+  // GlobalKey to manage the form state
   final _formKey = GlobalKey<FormState>();
+
+  // A boolean to manage loading state during login
   bool _isLoading = false;
 
+  // Function to handle login
   void _login() async {
+    // Validate the form
     if (_formKey.currentState!.validate()) {
+      // Show loading indicator
       setState(() {
         _isLoading = true;
       });
 
       try {
+        // Attempt to sign in the user with email and password
         UserCredential userCredential =
             await FirebaseAuth.instance.signInWithEmailAndPassword(
           email: _emailController.text,
           password: _passwordController.text,
         );
 
+        // Show success message
         ScaffoldMessenger.of(context).showSnackBar(
           const SnackBar(content: Text('Login successful!')),
         );
 
-        // นำผู้ใช้ไปที่หน้า HomePage
+        // Navigate to HomePage after successful login // นำผู้ใช้ไปที่หน้า HomePage
         Navigator.pushReplacement(
           context,
           MaterialPageRoute(builder: (context) => const HomePage()),
         );
       } catch (e) {
+        // Show error message if login fails
         if (mounted) {
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(content: Text('Login failed: ${e.toString()}')),
           );
         }
       } finally {
+        // Hide loading indicator
         setState(() {
           _isLoading = false;
         });
@@ -50,87 +64,100 @@ class _LoginPageState extends State<LoginPage> {
     }
   }
 
+  // Function to handle password reset
   void _forgotPassword() async {
+    // Check if email field is empty
     if (_emailController.text.isEmpty) {
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Please enter your email')),
       );
-      return;
+      return; // Exit the function if email is empty
     }
 
     try {
+      // Send password reset email
       await FirebaseAuth.instance.sendPasswordResetEmail(
         email: _emailController.text,
       );
 
+      // Show success message
       ScaffoldMessenger.of(context).showSnackBar(
         const SnackBar(content: Text('Password reset email sent!')),
       );
     } catch (e) {
+      // Show error message if sending reset email fails
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(content: Text('Failed to send reset email: $e')),
       );
     }
   }
 
+  // Building the UI for the LoginPage
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 246, 242, 242),
+      backgroundColor:
+          const Color.fromARGB(255, 246, 242, 242), // Set background color
       body: Center(
         child: Padding(
-          padding: const EdgeInsets.all(16.0),
+          padding: const EdgeInsets.all(16.0), // Padding around the form
           child: Form(
-            key: _formKey,
+            key: _formKey, // Assign the form key for validation
             child: Column(
-              mainAxisSize: MainAxisSize.min,
+              mainAxisSize: MainAxisSize.min, // Minimize the size of the column
               children: [
+                // Logo image at the top
                 Image.asset(
                   'assets/images/reminder.png',
-                  height: 150.0,
+                  height: 150.0, // Set image height
                 ),
-                const SizedBox(height: 32.0),
+                const SizedBox(height: 32.0), // Space between widgets
+                // Email text field
                 _buildTextField(
                   controller: _emailController,
                   label: 'Email',
                   keyboardType: TextInputType.emailAddress,
                   validator: (value) {
                     if (value!.isEmpty) {
-                      return 'Please enter your email';
+                      return 'Please enter your email'; // Error message if email is empty
                     }
-                    return null;
+                    return null; // No error
                   },
                 ),
-                const SizedBox(height: 16.0),
+                const SizedBox(height: 16.0), // Space between widgets
+                // Password text field
                 _buildTextField(
                   controller: _passwordController,
                   label: 'Password',
-                  obscureText: true,
+                  obscureText: true, // Hide password input
                   validator: (value) {
                     if (value!.isEmpty) {
-                      return 'Please enter your password';
+                      return 'Please enter your password'; // Error message if password is empty
                     }
-                    return null;
+                    return null; // No error
                   },
                 ),
-                const SizedBox(height: 8.0),
+                const SizedBox(height: 8.0), // Space between widgets
+                // "Forgot Password?" button
                 Align(
                   alignment: Alignment.centerRight,
                   child: TextButton(
-                    onPressed: _forgotPassword,
+                    onPressed:
+                        _forgotPassword, // Call the forgot password function
                     child: const Text(
                       'Forgot Password?',
                       style: TextStyle(
-                        color: Color.fromARGB(255, 0, 0, 0),
+                        color: Color.fromARGB(255, 0, 0, 0), // Text color
                       ),
                     ),
                   ),
                 ),
-                const SizedBox(height: 16.0),
+                const SizedBox(height: 16.0), // Space between widgets
+                // Show loading indicator or login button
                 _isLoading
-                    ? const CircularProgressIndicator()
+                    ? const CircularProgressIndicator() // Show loading indicator if logging in
                     : ElevatedButton(
-                        onPressed: _login,
+                        onPressed: _login, // Call the login function
                         style: ElevatedButton.styleFrom(
                           padding: const EdgeInsets.symmetric(
                               horizontal: 32.0, vertical: 12.0),
@@ -142,11 +169,13 @@ class _LoginPageState extends State<LoginPage> {
                           'Log in',
                           style: TextStyle(
                             fontSize: 18.0,
-                            color: Color.fromARGB(255, 0, 0, 0),
+                            color: Color.fromARGB(
+                                255, 0, 0, 0), // Button text color
                           ),
                         ),
                       ),
-                const SizedBox(height: 20.0),
+                const SizedBox(height: 20.0), // Space between widgets
+                // "Register" button to navigate to the registration page
                 TextButton(
                   onPressed: () {
                     Navigator.push(
@@ -157,7 +186,7 @@ class _LoginPageState extends State<LoginPage> {
                   child: const Text(
                     'Don\'t have an account? Register',
                     style: TextStyle(
-                      color: Color.fromARGB(255, 0, 0, 0),
+                      color: Color.fromARGB(255, 0, 0, 0), // Text color
                     ),
                   ),
                 ),
@@ -169,28 +198,33 @@ class _LoginPageState extends State<LoginPage> {
     );
   }
 
+  // Function to build a text field
   Widget _buildTextField({
-    required TextEditingController controller,
-    required String label,
-    bool obscureText = false,
-    TextInputType keyboardType = TextInputType.text,
-    required String? Function(String?) validator,
+    required TextEditingController controller, // Controller for text input
+    required String label, // Label for the text field
+    bool obscureText = false, // Whether to obscure text (for passwords)
+    TextInputType keyboardType = TextInputType.text, // Type of keyboard to use
+    required String? Function(String?)
+        validator, // Validator function for input
   }) {
     return TextFormField(
-      controller: controller,
+      controller: controller, // Assign the controller
       decoration: InputDecoration(
-        labelText: label,
+        labelText: label, // Set the label
         labelStyle: const TextStyle(
-            color: Color.fromARGB(255, 0, 0, 0)), // สีตัวอักษร Label
+            color: Color.fromARGB(
+                255, 0, 0, 0)), // Label text color / สีตัวอักษร Label
         border: OutlineInputBorder(
-          borderRadius: BorderRadius.circular(8.0),
+          borderRadius:
+              BorderRadius.circular(8.0), // Rounded corners for the border
         ),
       ),
-      obscureText: obscureText,
-      keyboardType: keyboardType,
+      obscureText: obscureText, // Whether to obscure text
+      keyboardType: keyboardType, // Set keyboard type
       style: const TextStyle(
-          color: Color.fromARGB(255, 0, 0, 0)), // สีตัวอักษรใน TextField
-      validator: validator,
+          color: Color.fromARGB(255, 0, 0,
+              0)), // Text color in the TextField // สีตัวอักษรใน TextField
+      validator: validator, // Assign the validator function
     );
   }
 }
