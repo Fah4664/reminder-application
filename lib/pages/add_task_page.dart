f1488ebd8da12ec17b0d8a68f6d3647fa1bba0b2
Ckew2565 <kewalin2284@gmail.com>
2024-10-12 16:17:02 +0700

    Update add_task_page.dart

--- lib/pages/add_task_page.dart
+++ lib/pages/add_task_page.dart
@@ -1,15 +1,16 @@
 import 'package:flutter/material.dart';
-import '../detail_task_from/task_form.dart';
+import '../detail_task_from/task_form.dart'; // Importing the TaskForm widget
 
+// Stateless widget for adding a new task
 class AddTaskPage extends StatelessWidget {
-  const AddTaskPage({super.key});
+  const AddTaskPage({super.key}); // Constructor with a key parameter
 
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
       body: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: TaskForm(),
+        padding: EdgeInsets.all(16.0), // Adds padding around the TaskForm
+        child: TaskForm(), // The TaskForm widget for task input
       ),
     );
   }
