4573301d3b6df9388dc8bb95b1c0b5847bdd7778
Ckew2565 <kewalin2284@gmail.com>
2024-10-12 16:10:31 +0700

    Update task_title.dart

--- lib/detail_task_from/task_title.dart
+++ lib/detail_task_from/task_title.dart
@@ -1,23 +1,27 @@
 // task_form_fields.dart
 import 'package:flutter/material.dart';
 
+// Widget for inputting task title and description.
 class TaskTitle extends StatelessWidget {
   final TextEditingController titleController;
   final TextEditingController descriptionController;
-  
+
+  // Constructor accepting required controllers for title and description.
   const TaskTitle({
-    super.key, 
-    required this.titleController, 
+    super.key,
+    required this.titleController,
     required this.descriptionController,
-  }); 
+  });
 
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
+        // Text field for task title.
         TextField(
-          controller: titleController,
+          controller:
+              titleController, // Uses the provided controller for managing input.
           decoration: const InputDecoration(
             hintText: 'Task Name...',
             hintStyle: TextStyle(
@@ -34,15 +38,17 @@ class TaskTitle extends StatelessWidget {
           thickness: 1,
         ),
         TextField(
-          controller: descriptionController,
+          controller:
+              descriptionController, // Uses the provided controller for managing input.
           decoration: const InputDecoration(
-            hintText: 'Task Description...',
+            hintText:
+                'Task Description...', // Placeholder text for description.
             hintStyle: TextStyle(
               fontSize: 18,
               color: Color(0xFFd0d0d0),
             ),
-            contentPadding: EdgeInsets.zero,
-            border: InputBorder.none,
+            contentPadding: EdgeInsets.zero, // No padding around the content.
+            border: InputBorder.none, // No border for the text field.
           ),
           style: const TextStyle(fontSize: 18),
         ),
