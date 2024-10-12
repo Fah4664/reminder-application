3fda8466e921c7ede8b7b267d2153310732798e6
Fah4664 <warapornzz4664@gmail.com>
2024-10-12 19:52:03 +0700

    Update task_details_popup.dart

--- lib/task_details_popup.dart
+++ lib/task_details_popup.dart
@@ -12,58 +12,49 @@ import '../utils/date_utils.dart'; // Utility for date formatting
 void showTaskDetailsPopup(BuildContext context, Task task, int index) {
   // Determine the dialog color based on the task color, default to a light gray
   Color dialogColor = task.color != null
-      ? colorFromString(
-          task.color!) // Convert the color string to a Color object
+      ? colorFromString(task.color!) // Convert the color string to a Color object
       : const Color(0xFFede3e3); // Default color if no color is provided
 
   // Show a dialog
   showDialog(
     context: context,
     builder: (BuildContext context) {
+      // Adjust button text based on task completion status
+      String buttonText = task.isCompleted ? 'Unmark as Completed' : 'Mark as Completed';
+
       return AlertDialog(
         contentPadding: const EdgeInsets.all(15.0), // Padding around content
         shape: RoundedRectangleBorder(
-          borderRadius:
-              BorderRadius.circular(5.0), // Rounded corners for the dialog
+          borderRadius: BorderRadius.circular(5.0), // Rounded corners for the dialog
         ),
-        backgroundColor:
-            dialogColor, // Set background color to the determined dialog color // ใช้สีที่รับเข้ามาจาก Task
+        backgroundColor: dialogColor, // Set background color to the determined dialog color
         content: SizedBox(
-          width: MediaQuery.of(context).size.width *
-              0.8, // Set width to 80% of screen width
-          height: MediaQuery.of(context).size.height *
-              0.3, // Set height to 30% of screen height
+          width: MediaQuery.of(context).size.width * 0.8, // Set width to 80% of screen width
+          height: MediaQuery.of(context).size.height * 0.3, // Set height to 30% of screen height
           child: Column(
-            mainAxisSize:
-                MainAxisSize.min, // Allow the column to take minimum space
-            crossAxisAlignment:
-                CrossAxisAlignment.start, // Align children to the start
+            mainAxisSize: MainAxisSize.min, // Allow the column to take minimum space
+            crossAxisAlignment: CrossAxisAlignment.start, // Align children to the start
             children: <Widget>[
               Row(
-                mainAxisAlignment: MainAxisAlignment
-                    .spaceBetween, // Space between title and edit button
+                mainAxisAlignment: MainAxisAlignment.spaceBetween, // Space between title and edit button
                 children: [
                   Text(
                     task.title, // Display task title
                     style: const TextStyle(
                       fontSize: 22, // Font size for the title
                       fontWeight: FontWeight.bold, // Bold text
-                      color: Color(
-                          0xFF000000), // Black color for text // เปลี่ยนสีข้อความให้เหมาะสมกับพื้นหลัง
+                      color: Color(0xFF000000), // Black color for text
                     ),
                   ),
                   IconButton(
-                    // Button to navigate to the edit task page // แสดงหน้าจอการแก้ไข task หรือทำการอัปเดต task ตรงนี้
-                    icon: SvgPicture.asset(
-                        'assets/icons/pencil.svg'), // Edit icon
+                    // Button to navigate to the edit task page
+                    icon: SvgPicture.asset('assets/icons/pencil.svg'), // Edit icon
                     onPressed: () {
                       Navigator.of(context).pop(); // Close dialog
                       Navigator.push(
                         context,
                         MaterialPageRoute(
-                          builder: (context) => EditTaskPage(
-                              task:
-                                  task), // Navigate to EditTaskPage with task data
+                          builder: (context) => EditTaskPage(task: task), // Navigate to EditTaskPage with task data
                         ),
                       );
                     },
@@ -80,50 +71,39 @@ void showTaskDetailsPopup(BuildContext context, Task task, int index) {
               ),
               const SizedBox(height: 10), // Spacing below the description
               Row(
-                mainAxisAlignment: MainAxisAlignment
-                    .start, // Align progress bar to the start // จัดเรียงให้เริ่มจากซ้าย
+                mainAxisAlignment: MainAxisAlignment.start, // Align progress bar to the start
                 children: [
                   Container(
                     width: 200, // Fixed width for progress bar background
                     height: 15, // Fixed height for progress bar
                     decoration: BoxDecoration(
                       color: const Color(0xFFd0d0d0), // Light gray background
-                      borderRadius:
-                          BorderRadius.circular(15.0), // Rounded corners
+                      borderRadius: BorderRadius.circular(15.0), // Rounded corners
                     ),
                     child: Align(
-                      alignment: Alignment
-                          .centerLeft, // Align the progress indicator to the left
+                      alignment: Alignment.centerLeft, // Align the progress indicator to the left
                       child: Container(
                         width: (MediaQuery.of(context).size.width - 100) *
-                            (task.sliderValue *
-                                100 /
-                                150), // Calculate the width of the progress based on slider value
+                            (task.sliderValue * 100 / 150), // Calculate the width of the progress based on slider value
                         height: 15, // Fixed height for progress indicator
                         decoration: BoxDecoration(
-                          color: const Color(
-                              0xFF717273), // Dark gray for the progress bar
-                          borderRadius:
-                              BorderRadius.circular(15.0), // Rounded corners
+                          color: const Color(0xFF717273), // Dark gray for the progress bar
+                          borderRadius: BorderRadius.circular(15.0), // Rounded corners
                         ),
                       ),
                     ),
                   ),
-                  const SizedBox(
-                      width:
-                          10), // Spacing between the progress bar and percentage text
+                  const SizedBox(width: 10), // Spacing between the progress bar and percentage text
                   Text(
                     '${(task.sliderValue * 100).toStringAsFixed(0)}%', // Display the percentage of task completion
                     style: const TextStyle(
-                        fontSize: 16,
-                        color: Color(0xFF000000)), // Text style for percentage
+                        fontSize: 16, color: Color(0xFF000000)), // Text style for percentage
                   ),
                 ],
               ),
               const SizedBox(height: 10), // Spacing below the progress bar
               Text(
-                formatDateRange(task.startDateTime,
-                    task.endDateTime), // Format and display the date range
+                formatDateRange(task.startDateTime, task.endDateTime), // Format and display the date range
                 style: const TextStyle(
                     fontSize: 16, // Font size for date range
                     color: Color(0xFF000000)), // Black color for text
@@ -135,42 +115,38 @@ void showTaskDetailsPopup(BuildContext context, Task task, int index) {
           Row(
             children: [
               IconButton(
-                icon: const Icon(Icons.arrow_back,
-                    color: Color(0xFF000000)), // Back arrow icon
+                icon: const Icon(Icons.arrow_back, color: Color(0xFF000000)), // Back arrow icon
                 onPressed: () {
                   Navigator.of(context).pop(); // Close dialog
-                },
+                }, 
               ),
               const Spacer(), // Add space between back button and action buttons
               TextButton(
                 onPressed: () {
-                  // Mark the task as completed
-                  Provider.of<TaskProvider>(context, listen: false)
-                      .markTaskAsCompleted(task);
+                  // Toggle completion status
+                  if (task.isCompleted) {
+                    Provider.of<TaskProvider>(context, listen: false).unmarkTaskAsCompleted(task);
+                  } else {
+                    Provider.of<TaskProvider>(context, listen: false).markTaskAsCompleted(task);
+                  }
                   Navigator.of(context).pop(); // Close dialog
                 },
                 style: TextButton.styleFrom(
-                  foregroundColor:
-                      const Color(0xFF000000), // Color for button text
-                  backgroundColor: Color.fromRGBO(
-                      208, 208, 208, 0.8), // Light gray with transparency
+                  foregroundColor: const Color(0xFF000000), // Color for button text
+                  backgroundColor: Color.fromRGBO(208, 208, 208, 0.8), // Light gray with transparency
                   shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(
-                        5.0), // Rounded corners for the button
+                    borderRadius: BorderRadius.circular(5.0), // Rounded corners for the button
                   ),
-                  padding: const EdgeInsets.symmetric(
-                      horizontal: 4.0, vertical: 0.5), // Padding for the button
+                  padding: const EdgeInsets.symmetric(horizontal: 4.0, vertical: 0.5), // Padding for the button
                 ),
-                child: const Text('Mark as Completed'), // Button text
+                child: Text(buttonText), // Button text
               ),
               const Spacer(), // Add space between action buttons
               IconButton(
-                icon: SvgPicture.asset(
-                    'assets/icons/trash-xmark.svg'), // Trash icon for deleting the task
+                icon: SvgPicture.asset('assets/icons/trash-xmark.svg'), // Trash icon for deleting the task
                 onPressed: () {
                   // Remove the task
-                  Provider.of<TaskProvider>(context, listen: false)
-                      .removeTask(task);
+                  Provider.of<TaskProvider>(context, listen: false).removeTask(task);
                   Navigator.of(context).pop(); // Close dialog
                 },
               ),
