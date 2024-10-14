01d3e2842f0ec67eed896c8c0d021576864e6e11
Fah4664 <warapornzz4664@gmail.com>
2024-10-14 15:22:42 +0700

    Update splash_page.dart

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,31 +1,32 @@
 import 'package:flutter/material.dart';
-import 'dart:async'; // Import Dart's asynchronous library
-import 'login_page.dart'; // Import the LoginPage to navigate after the splash screen
+import 'dart:async';
+import 'login_page.dart';
 
-// This class represents the splash page
+// This class represents the splash page.
 class SplashPage extends StatefulWidget {
-  const SplashPage({super.key}); // Constructor
+  const SplashPage({super.key});
 
   @override
-  // Creates the state for the splash page
-  _SplashPageState createState() => _SplashPageState();
+  // Creates the state for the splash page.
+  SplashPageState createState() => SplashPageState();
 }
 
-// State class for SplashPage
-class _SplashPageState extends State<SplashPage> {
+// State class for SplashPage.
+class SplashPageState extends State<SplashPage> {
   @override
   void initState() {
-    super.initState(); // Call the superclass's initState method
-    // Set a timer to navigate to the login page after 3 seconds
+    // Call the superclass's initState method.
+    super.initState();
+    // Set a timer to navigate to the login page after 3 seconds.
     Timer(const Duration(seconds: 3), () {
       if (mounted) {
-        // Check if the widget is still mounted in the widget tree // ตรวจสอบว่าผู้ใช้ยังอยู่ในต้นไม้ของ widget
+        // Check if the widget is still mounted in the widget tree.
         Navigator.pushAndRemoveUntil(
           context,
-          MaterialPageRoute(
-              builder: (context) => LoginPage()), // Navigate to LoginPage
-          (Route<dynamic> route) =>
-              false, // Remove all previous routes from the stack // ลบหน้าทั้งหมดใน stack
+          // Navigate to LoginPage.
+          MaterialPageRoute(builder: (context) => LoginPage()),
+          // Remove all previous routes from the stack.
+          (Route<dynamic> route) => false,
         );
       }
     });
@@ -35,12 +36,12 @@ class _SplashPageState extends State<SplashPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor:
-          Colors.white, // Set the background color of the splash page
+        Colors.white,
       body: Center(
         child: Image.asset(
-          'assets/images/reminder.png', // Specify the path of the image asset  // ระบุเส้นทางไฟล์ของรูปภาพ
-          width: 200, // Set the width of the image
-          height: 200, // Set the height of the image
+          'assets/images/reminder.png',
+          width: 200,
+          height: 200, 
         ),
       ),
     );
