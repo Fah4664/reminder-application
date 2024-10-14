4602e1113ea157ca8266e6c83540b754047d51cb
Fah4664 <warapornzz4664@gmail.com>
2024-10-14 15:22:36 +0700

    Update register_page.dart

--- lib/pages/register_page.dart
+++ lib/pages/register_page.dart
@@ -3,79 +3,74 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'login_page.dart';
 
-// This class represents the registration page
+// This class represents the registration page.
 class RegisterPage extends StatefulWidget {
   const RegisterPage({super.key});
 
   @override
-  // Creates the state for the registration page
-  _RegisterPageState createState() => _RegisterPageState();
+  // Creates the state for the registration page.
+  RegisterPageState createState() => RegisterPageState();
 }
 
-// State class for RegisterPage
-class _RegisterPageState extends State<RegisterPage> {
-  // Controllers to handle user input
+// State class for RegisterPage.
+class RegisterPageState extends State<RegisterPage> {
+  // Controllers to handle user input.
   final _nameController = TextEditingController();
   final _emailController = TextEditingController();
   final _passwordController = TextEditingController();
-
-  // Key for form validation
+  // Key for form validation.
   final _formKey = GlobalKey<FormState>();
-
-  // Loading state to show progress indicator during registration
+  // Loading state to show progress indicator during registration.
   bool _isLoading = false;
 
-  // Function to handle registration logic
+  // Function to handle registration logic.
   void _register() async {
-    // Validate the form input
+    // Validate the form input.
     if (_formKey.currentState!.validate()) {
-      // Set loading state to true
+      // Set loading state to true.
       setState(() {
         _isLoading = true;
       });
 
       try {
-        // Create a new user with email and password
-        final userCredential =
-            await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        // Create a new user with email and password.
+        final userCredential = await FirebaseAuth.instance.createUserWithEmailAndPassword(
           email: _emailController.text,
           password: _passwordController.text,
         );
-
-        // Get the user object and UID
+        // Get the user object and UID.
         final User? user = userCredential.user;
         final String? uid = user?.uid;
 
-        // If UID is not null, save user's name and email to Firestore
+        // If UID is not null, save user's name and email to Firestore.
         if (uid != null) {
-          // เก็บชื่อและเมลลงใน Cloud Firestore
+          // เก็บชื่อและเมลลงใน Cloud Firestore.
           await FirebaseFirestore.instance.collection('usersID').doc(uid).set({
             'name': _nameController.text,
             'email': _emailController.text,
           });
         }
 
-        // Show success message and navigate to LoginPage
+        // Show success message and navigate to LoginPage.
         if (mounted) {
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(content: Text('Registration successful!')),
           );
-
-          // Navigate to the login page // นำผู้ใช้ไปที่หน้า LoginPage
+          // Navigate to the login page.
           Navigator.pushReplacement(
             context,
             MaterialPageRoute(builder: (context) => LoginPage()),
           );
         }
       } catch (e) {
-        // Handle registration errors
+        // Handle registration errors.
         if (mounted) {
           ScaffoldMessenger.of(context).showSnackBar(
             SnackBar(content: Text('Registration failed: $e')),
           );
         }
       } finally {
-        // Reset loading state
+        // Reset loading state.
         if (mounted) {
           setState(() {
             _isLoading = false;
@@ -88,13 +83,13 @@ class _RegisterPageState extends State<RegisterPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      // Set background color for the registration page
+      // Set background color for the registration page.
       backgroundColor: const Color.fromARGB(255, 246, 242, 242),
       body: Center(
         child: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Form(
-            key: _formKey, // Assign the form key for validation
+            key: _formKey, // Assign the form key for validation.
             child: Column(
               mainAxisSize: MainAxisSize.min,
               children: [
@@ -104,7 +99,7 @@ class _RegisterPageState extends State<RegisterPage> {
                   height: 150.0,
                 ),
                 const SizedBox(height: 32.0),
-                // Name text field with validation
+                // Name text field with validation.
                 _buildTextField(
                   controller: _nameController,
                   label: 'Name',
@@ -112,11 +107,11 @@ class _RegisterPageState extends State<RegisterPage> {
                     if (value!.isEmpty) {
                       return 'Please enter your name';
                     }
-                    return null; // Valid input
+                    return null; // Valid input.
                   },
                 ),
                 const SizedBox(height: 16.0),
-                // Email text field with validation
+                // Email text field with validation.
                 _buildTextField(
                   controller: _emailController,
                   label: 'Email',
@@ -125,31 +120,31 @@ class _RegisterPageState extends State<RegisterPage> {
                     if (value!.isEmpty) {
                       return 'Please enter your email';
                     }
-                    return null; // Valid input
+                    return null; // Valid input.
                   },
                 ),
                 const SizedBox(height: 16.0),
-                // Password text field with validation
+                // Password text field with validation.
                 _buildTextField(
                   controller: _passwordController,
                   label: 'Password',
-                  obscureText: true, // Hide password input
+                  obscureText: true, // Hide password input.
                   validator: (value) {
                     if (value!.isEmpty) {
                       return 'Please enter your password';
                     }
-                    return null; // Valid input
+                    return null; // Valid input.
                   },
                 ),
                 const SizedBox(height: 32.0),
-                // Show loading indicator or register button
+                // Show loading indicator or register button.
                 _isLoading
                     ? const CircularProgressIndicator()
                     : ElevatedButton(
-                        onPressed: _register, // Call register function
+                        onPressed: _register, // Call register function.
                         style: ElevatedButton.styleFrom(
                           padding: const EdgeInsets.symmetric(
-                              horizontal: 32.0, vertical: 12.0),
+                            horizontal: 32.0, vertical: 12.0),
                           shape: RoundedRectangleBorder(
                             borderRadius: BorderRadius.circular(8.0),
                           ),
@@ -163,7 +158,7 @@ class _RegisterPageState extends State<RegisterPage> {
                         ),
                       ),
                 const SizedBox(height: 20.0),
-                // Text button to navigate to LoginPage
+                // Text button to navigate to LoginPage.
                 TextButton(
                   onPressed: () {
                     Navigator.push(
@@ -171,10 +166,10 @@ class _RegisterPageState extends State<RegisterPage> {
                       MaterialPageRoute(builder: (context) => LoginPage()),
                     );
                   },
-                  child: Text(
+                  child: const Text(
                     'Already have an account? Log in',
                     style: TextStyle(
-                      color: const Color.fromARGB(255, 0, 0, 0),
+                      color: Color.fromARGB(255, 0, 0, 0),
                     ),
                   ),
                 ),
@@ -186,7 +181,7 @@ class _RegisterPageState extends State<RegisterPage> {
     );
   }
 
-  // Function to build a text field with common properties
+  // Function to build a text field with common properties.
   Widget _buildTextField({
     required TextEditingController controller,
     required String label,
@@ -198,15 +193,15 @@ class _RegisterPageState extends State<RegisterPage> {
       controller: controller,
       decoration: InputDecoration(
         labelText: label,
-        labelStyle: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
+        labelStyle: const TextStyle(color:  Color.fromARGB(255, 0, 0, 0)),
         border: OutlineInputBorder(
           borderRadius: BorderRadius.circular(8.0),
         ),
       ),
-      obscureText: obscureText, // Hide text if true
-      keyboardType: keyboardType, // Set keyboard type
-      style: TextStyle(color: const Color.fromARGB(255, 0, 0, 0)),
-      validator: validator, // Attach validation function
+      obscureText: obscureText, // Hide text if true.
+      keyboardType: keyboardType, // Set keyboard type.
+      style: const TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+      validator: validator, // Attach validation function.
     );
   }
 }
