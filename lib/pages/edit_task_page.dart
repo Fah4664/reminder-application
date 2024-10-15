6e80039a016d70e602e6b0d8cd7f4620220552f1
Fah4664 <warapornzz4664@gmail.com>
2024-10-15 19:22:41 +0700

    Update edit_task_page.dart

--- lib/pages/edit_task_page.dart
+++ lib/pages/edit_task_page.dart
@@ -2,20 +2,20 @@ import 'package:flutter/material.dart';
 import '../models/task.dart';
 import '../detail_addtask_from/addtask_form.dart'; 
 
-// Stateless widget for editing an existing task
+// Stateless widget for editing an existing task.
 class EditTaskPage extends StatelessWidget {
-  // A Task object representing the task to be edited
+  // A Task object representing the task to be edited.
   final Task task;
-  // Constructor that requires a Task
+  // Constructor that requires a Task.
   const EditTaskPage({super.key, required this.task});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        // Adds padding around the TaskForm
+        // Adds padding around the TaskForm.
         padding: const EdgeInsets.all(16.0),
-        // Passes the task to TaskForm for editing
+        // Passes the task to TaskForm for editing.
         child: TaskForm(
           initialTask: task),
       ),
